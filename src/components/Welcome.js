998257194fbe41dee66a9ea588149b11cd592608
shindepriti <pritishinde34@gmail.com>
2020-05-31 16:33:18 +0530

    ADDED: Use Constructor in Class

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 class Welcome extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {date : new Date()};
+    }
+
     render(){
-        return <h2>Class {this.props.name}</h2>
+        return (
+            <div>
+                <h2>Class {this.props.name}</h2>
+                <h3>It Is {this.state.date.toLocaleTimeString()}.</h3>
+            </div>
+        )  
     }
 }
 export default Welcome
